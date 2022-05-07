5969b4e7c0942590aaa26824f4f6038654f694c8
xin <xin20wang@outlook.com>
2022-05-07 12:24:15 +0200

    Time fixed

--- src/data/dbConnection.js
+++ src/data/dbConnection.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-
+    timezone: 'UTC'
 })
 
 module.exports = pool;
